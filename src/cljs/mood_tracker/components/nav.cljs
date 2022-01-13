38873a28c8705f54dabf8238bb8575e1005d3d18
IngvarListard <vlzvoice@ya.ru>
2022-01-13 23:13:26 +0300

    remove old components

--- src/cljs/mood_tracker/components/nav.cljs
+++
@@ -1,9 +0,0 @@
-(ns mood-tracker.components.nav
-  (:require [helix.core :refer [defnc]]
-            [helix.dom :as d]))
-
-
-(defnc nav []
-  (d/nav {:class '[py-4 shadow]}
-         (d/div {:class '[container]}
-          (d/h2 {:class '[text-xl]} "Reports"))))
