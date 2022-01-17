be2a8f6edbd96061c86f244376c5d9f5b37f54e7
IngvarListard <vlzvoice@ya.ru>
2022-01-17 21:43:53 +0300

    create report dialog draft

--- src/cljs/good_mood/components/reports/report_dialog.cljs
+++ src/cljs/good_mood/components/reports/report_dialog.cljs
@@ -1,14 +1,64 @@
 (ns good-mood.components.reports.report-dialog
   (:require
    [reagent.core :as r]
-   [reagent-mui.material.dialog :refer [dialog]]))
+   [react]
+   [reagent-mui.material.dialog :refer [dialog]]
+   [reagent-mui.material.button :refer [button]]
+   [reagent-mui.material.icon-button :refer [icon-button]]
+   [reagent-mui.material.dialog-title :refer [dialog-title]]
+   [reagent-mui.material.slide :refer [slide]]
+   [reagent-mui.material.divider :refer [divider]]
+   [reagent-mui.material.app-bar :refer [app-bar]]
+   [reagent-mui.material.toolbar :refer [toolbar]]
+   [reagent-mui.material.typography :refer [typography]]
+   [reagent-mui.icons.close :refer [close] :rename {close close-icon}]
 
-(defn display-dialog [dialog-opened?]
-  (fn []
-      (println "dialog rendered")
-    (let [opened? @dialog-opened?]
-      (println "dialog rendered")
-      [dialog
-       {:full-screen true
-        :open opened?}
-       [:div "Hey yo"]])))
+   ))
+
+(def transition
+  "не работает
+  https://github.com/reagent-project/reagent/blob/c214466bbcf099eafdfe28ff7cb91f99670a8433/examples/react-mde/src/example/core.cljs
+  forward-ref example"
+  (react/forwardRef
+    (fn [props ref]
+      (let [props (assoc (js->clj props) :ref ref)]
+        (set! (.-direction props) "up")
+        (r/create-element slide props)))))
+
+(defn top-bar [{:keys [close]}]
+  [app-bar {:sx {:position "relative"}}
+   [toolbar
+    [icon-button {:edge "start"
+                  :color "inherit"
+                  :on-click close
+                  :aria-label "close"}
+     [close-icon]]
+    [typography {:sx {:ml 2 :flex 1}
+                 :variant "h6"
+                 :component "div"}
+     "Sound"]
+    [button {:auto-focus true
+             :color "inherit"
+             :on-click close}
+     "Save"]]])
+
+(defn dialog-body [])
+
+(defn create-report-dialog []
+  (let [opened? (r/atom false)
+        open #(reset! opened? true)
+        close #(reset! opened? false)]
+    (fn []
+      (println "dialog rendered" @opened?)
+      [:div
+       [button {:variant "contained"
+                :color "primary"
+                :on-click open
+                :on-close close}
+        "Create report"]
+       [dialog
+        {:full-screen true
+         :open @opened?}
+        [top-bar {:close close}]
+        [:div "Hey yo"]
+        ]])))
--- src/cljs/good_mood/pages/reports/index.cljs
+++ src/cljs/good_mood/pages/reports/index.cljs
@@ -1,7 +1,7 @@
 (ns good-mood.pages.reports.index
   (:require
    [good-mood.components.reports.reports-table :refer [display-reports-table]]
-   [good-mood.components.reports.report-dialog :refer [display-dialog]]
+   [good-mood.components.reports.report-dialog :refer [create-report-dialog]]
    [re-frame.core :as rf]
    [reagent.core :as r]
    [reagent-mui.material.button :refer [button]]
@@ -11,17 +11,20 @@
 (defn reports-page []
   (let [loading (rf/subscribe [:common/loading])
         reports (rf/subscribe [:common/reports])]
-    (fn []
-      (let [dialog? (r/atom false)]
-        (display-dialog dialog?)
-        [:section.section>div.container>div.content
-         [button {:variant "contained"
-                  :color "primary"
-                  :on-click #(rf/dispatch [:common/fetch-reports])}
-          "Get reports"]
-         [button {:variant "contained"
-                  :color "primary"
-                  :on-click #(swap! dialog? (fn [] true))}
-          "Create report"]
-         (when @loading [:div "Loading..."])
-         [paper (display-reports-table @reports)]]))))
+    [:section.section>div.container>div.content
+       [button {:variant "contained"
+                :color "primary"
+                :on-click #(rf/dispatch [:common/fetch-reports])}
+        "Get reports"]
+       [create-report-dialog]
+       (when @loading [:div "Loading..."])
+       [paper (display-reports-table @reports)]]))
+
+
+(comment
+
+  (let [dialog? (r/atom false)]
+    (println "DIALOG 1" @dialog?)
+    (reset! dialog? true)
+    (println "DIALOG 2" @dialog?)
+))
